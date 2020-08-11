e62e3dbbbb2ae2c453e10110bb14a4235c76f89d
Shubham Abnave <abnaveshubham111@.com>
2020-08-11 11:49:59 +0530

    checking datbase connection

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const path= require('path');
 const cors =require('cors');
 const http=require('http');
 
+const mongoose = require('mongoose');
+
+
 const app = express();
 
 const port =process.env.PORT || 8080;
+const db= "mongodb://216.10.242.121:27017";
+const conn = mongoose.createConnection(db);
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db, function(err){
+    if(err){
+        console.error('Error! ' + err)
+    } else {
+      console.log('Connected to mongodb')      
+    }
+});
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -35,8 +49,8 @@ app.use((req, res, next) => {
 
 
 
-//app.use(express.static(path.join(__dirname,'public')));
-//app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'public/index.html')));
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/*',(req,res)=> res.sendFile(path.join(__dirname,'public/index.html')));
 
 
 
@@ -46,5 +60,10 @@ app.get('/',function(req, res){
 app.listen(port, function(){
     console.log("Server running on localhost:" + port);
 });
+
+
+
+
+
 /*const server=http.createServer(app);
 server.listen(port,()=>console.log("Running"));*/
